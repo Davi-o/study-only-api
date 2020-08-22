9b7174ddd7ceb780fdbe32e4a188f0ced1e03e15
oDavi <alvesdelima.davi@gmail.com>
2020-08-22 00:30:26 -0300

    Refactoring the directories organization

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = express.Router();
 
-const UserController = require('./controllers/UserController');
+const UserController = require('../controllers/UserController');
 
 routes.post('/new-user', UserController.registerNewUser);
 
